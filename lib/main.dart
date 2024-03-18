352f12c44089fa5376f789668d181f7d38700d7b
Arvin Visi <arvinvisi03@gmail.com>
2024-03-18 10:56:33 +0330

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: TextTheme(headlineLarge: TextStyle(color: Colors.white)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: Text(
+        'hello',
+        style: Theme.of(context).textTheme.headlineLarge,
+      )),
+    );
+  }
+}
