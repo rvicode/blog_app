d49e0db37ef904bee6c22c3dd1b19b791637c832
Arvin Veysi <arvinvisi03@gmail.com>
2024-03-24 14:13:33 +0330

    edd navbar project

--- lib/main.dart
+++ lib/main.dart
@@ -5,32 +5,64 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const String defaultfontfamily = 'Avenir';
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const primaryTextColor = Color(0xff0D253C);
+    const secoundaryTextColor = Color(0xff2D4379);
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        textTheme: TextTheme(headlineLarge: TextStyle(color: Colors.white)),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: const TextTheme(
+            titleMedium: TextStyle(
+                fontFamily: defaultfontfamily,
+                color: secoundaryTextColor,
+                fontSize: 16),
+            titleLarge: TextStyle(
+                fontFamily: defaultfontfamily,
+                color: primaryTextColor,
+                fontWeight: FontWeight.bold)),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: Text(
-        'hello',
-        style: Theme.of(context).textTheme.headlineLarge,
-      )),
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(32, 24, 32, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Hi, Arvin!',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  Image.asset(
+                    'assets/img/icons/notification.png',
+                    width: 24,
+                    height: 24,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+              child: Text('Explore todays',
+                  style: Theme.of(context).textTheme.titleLarge),
+            ),
+
+          ])),
     );
   }
 }
