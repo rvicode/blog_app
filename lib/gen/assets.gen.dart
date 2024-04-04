fc3971c0460fcea8499381dd25084c1f2bdb4ffe
Arvin Veysi <arvinvisi03@gmail.com>
2024-04-05 00:41:18 +0330

    add svg file in assets and create in fluttergen

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -50,6 +50,9 @@ class $AssetsImgIconsGen {
   AssetGenImage get google =>
       const AssetGenImage('assets/img/icons/Google.png');
 
+  /// File path: assets/img/icons/Grid.svg
+  SvgGenImage get grid => const SvgGenImage('assets/img/icons/Grid.svg');
+
   /// File path: assets/img/icons/Home.png
   AssetGenImage get home => const AssetGenImage('assets/img/icons/Home.png');
 
@@ -62,6 +65,9 @@ class $AssetsImgIconsGen {
   /// File path: assets/img/icons/Search.png
   AssetGenImage get search =>
       const AssetGenImage('assets/img/icons/Search.png');
+
+  /// File path: assets/img/icons/Table.svg
+  SvgGenImage get table => const SvgGenImage('assets/img/icons/Table.svg');
 
   /// File path: assets/img/icons/Twitter.png
   AssetGenImage get twitter =>
@@ -98,10 +104,12 @@ class $AssetsImgIconsGen {
         articles,
         facebook,
         google,
+        grid,
         home,
         logo,
         menu,
         search,
+        table,
         twitter,
         bilakh,
         category1,
