d86a8721ba36449945b2c53b7fa8d7cba246a74a
Arvin Veysi <arvinvisi03@gmail.com>
2024-04-10 15:29:40 +0330

    add MainScreen in file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:blog_app/gen/fonts.gen.dart';
-import 'package:blog_app/screens/splash_screen/splash_screen.dart';
+import 'package:blog_app/screens/article_screen/article_screen.dart';
+import 'package:blog_app/screens/home_screen/home_screen.dart';
+import 'package:blog_app/screens/profile_screen/profile_screen.dart';
+import 'package:blog_app/screens/search_screen/search_screen.dart';
+import 'package:blog_app/widgets/bottom_navigation_widget.dart';
 import 'package:blog_app/widgets/theme_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
@@ -30,6 +36,56 @@ class MyApp extends StatelessWidget {
               secoundaryTextColor, primaryTextColor, defaultfontfamily),
           useMaterial3: true,
         ),
-        home: const SplashScreen());
+        home: const MainScreen());
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+const int homeIndex = 0;
+const int articleIndex = 1;
+const int searchIndex = 2;
+const int profileIndex = 3;
+const double bottomNavigationHight = 65;
+
+class _MainScreenState extends State<MainScreen> {
+  int selectScreenIndex = homeIndex;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            bottom: bottomNavigationHight,
+            right: 0,
+            left: 0,
+            child: IndexedStack(
+              index: selectScreenIndex,
+              children: [
+                MyHomePage(),
+                ArticleScreen(),
+                SearchScreen(),
+                ProfileScreen()
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: BottomNavigation(onTap: (int index) {
+                    setState(() {
+            selectScreenIndex = index;
+                    });
+                  }, selectedIndex: selectScreenIndex,),
+          ),
+        ],
+      ),
+    );
   }
 }
