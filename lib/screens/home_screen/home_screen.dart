ec43bc151a884d2a6eb8b317cf64084711e1d5dd
Arvin Veysi <arvinvisi03@gmail.com>
2024-04-10 15:31:03 +0330

    delete Stack to file home_screen

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -3,7 +3,6 @@ import 'package:blog_app/gen/assets.gen.dart';
 import 'package:blog_app/screens/home_screen/widgets/category_widget.dart';
 import 'package:blog_app/screens/home_screen/widgets/post_widget.dart';
 import 'package:blog_app/screens/home_screen/widgets/story_image.dart';
-import 'package:blog_app/widgets/bottom_navigation_widget.dart';
 import 'package:flutter/material.dart';
 
 
@@ -17,105 +16,99 @@ class MyHomePage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
           backgroundColor: Colors.white,
-          body: Stack(
-            children: [
-              SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 65),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(32, 24, 32, 0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 65),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(32, 24, 32, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Hi, Arvin!',
+                            style: textTheme.titleMedium,
+                          ),
+                          Assets.img.icons.notification
+                              .image(width: 34, height: 34),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+                      child: Text('Explore today`s',
+                          style: textTheme.titleLarge),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    SizedBox(
+                      height: 100,
+                      width: MediaQuery.of(context).size.width,
+                      child: ListView.builder(
+                        padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: storys.length,
+                        itemBuilder: (context, index) {
+                          final story = storys[index];
+                          return Column(
                             children: [
+                              story.isViewed
+                                  ? storyImageViewed(story)
+                                  : storyImageNormal(story),
+                              const SizedBox(
+                                height: 10,
+                              ),
                               Text(
-                                'Hi, Arvin!',
-                                style: textTheme.titleMedium,
+                                story.name,
+                                style: textTheme.bodyMedium,
                               ),
-                              Assets.img.icons.notification
-                                  .image(width: 34, height: 34),
                             ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
-                          child: Text('Explore today`s',
-                              style: textTheme.titleLarge),
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        SizedBox(
-                          height: 100,
-                          width: MediaQuery.of(context).size.width,
-                          child: ListView.builder(
-                            padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
-                            scrollDirection: Axis.horizontal,
-                            itemCount: storys.length,
-                            itemBuilder: (context, index) {
-                              final story = storys[index];
-                              return Column(
-                                children: [
-                                  story.isViewed
-                                      ? storyImageViewed(story)
-                                      : storyImageNormal(story),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                  Text(
-                                    story.name,
-                                    style: textTheme.bodyMedium,
-                                  ),
-                                ],
-                              );
-                            },
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        const CategoryList(),
-                        SizedBox(
-                          height: 500,
-                          width: double.infinity,
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.only(right: 24, left: 32),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      'Laset News',
-                                      style: textTheme.titleLarge,
-                                    ),
-                                    TextButton(
-                                        onPressed: () {},
-                                        child: Text(
-                                          'More',
-                                          style: textTheme.titleMedium!.apply(
-                                              color: const Color(0xff376AED),
-                                              fontSizeFactor: 0.9),
-                                        ))
-                                  ],
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const CategoryList(),
+                    SizedBox(
+                      height: 500,
+                      width: double.infinity,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(right: 24, left: 32),
+                            child: Row(
+                              mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Laset News',
+                                  style: textTheme.titleLarge,
                                 ),
-                              ),
-                              Posts(
-                                textTheme: textTheme,
-                              ),
-                            ],
+                                TextButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'More',
+                                      style: textTheme.titleMedium!.apply(
+                                          color: const Color(0xff376AED),
+                                          fontSizeFactor: 0.9),
+                                    ))
+                              ],
+                            ),
                           ),
-                        )
-                      ]),
-                ),
-              ),
-              const Positioned(
-                  bottom: 0, left: 0, right: 0, child: BottomNavigation())
-            ],
+                          Posts(
+                            textTheme: textTheme,
+                          ),
+                        ],
+                      ),
+                    )
+                  ]),
+            ),
           )),
     );
   }
